a42e9491741da0f4d8a72a7bbf643b395ddf5649
ChitraR08 <ramichitra93@gmail.com>
2021-12-21 16:00:49 -0600

    New Test

---
+++ FBProject/src/Exam2/CharToStringExample1.java
@@ -0,0 +1,8 @@
+package Exam2;
+
+public class CharToStringExample1{  
+public static void main(String args[]){  
+char c='S';  
+String s=String.valueOf(c);  
+System.out.println("String is: "+s);  
+}}  
---
+++ FBProject/src/Exam2/IntToStringExample1.java
@@ -0,0 +1,9 @@
+package Exam2;
+
+public class IntToStringExample1{  
+public static void main(String args[]){  
+int i=200;  
+String s=String.valueOf(i);  
+System.out.println(i+100);
+System.out.println(s+100);
+}}  
